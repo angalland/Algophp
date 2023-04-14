b42a1f315d34f4a79507d65f62d797b51d08b70b
angalland <angalland@laposte.net>
2023-04-14 11:51:53 +0200

    ajout d'une boucle

---
+++ exo10.php
@@ -0,0 +1,19 @@
+<h1>Exercice 10</h1>
+
+<p>A partir d’un montant à payer et d’une somme versée pour régler un achat, écrire l’algorithme qui 
+affiche à un utilisateur un rendu de monnaie en nombre de billets de 10 € et 5 €, de pièces de 2 € et 
+1 €.</p>
+
+<h2>Resultat</h2>
+
+<?php
+
+$montpaye = 152;
+$montverse = 200;
+$restepaye = $montverse - $montpaye;
+
+echo "$restepaye";
+
+$billet = array[10, 5, 2, 1]
+
+for()
