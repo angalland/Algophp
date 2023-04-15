98df9f80a61a8746a27a6974a72cd21a80efc4fb
angalland <angalland@laposte.net>
2023-04-15 16:10:43 +0200

    ajout exo12

---
+++ exo12.php
@@ -0,0 +1,14 @@
+<h1>Exercice 12</h1>
+
+<p>A partir d’une fonction personnalisée et à partir d’un tableau de prénoms et de langue associée 
+(tableau contenant clé et valeur), dire bonjour aux différentes personnes dans leur langue 
+respective (français ➔ « Salut », anglais ➔ « Hello », espagnol ➔ « Hola »)
+</p>
+
+<h2>Resultat</h2>
+
+<?php
+
+$people = array("Mickaël"->FRA, "Virgile"->ESP, "Marie-claire"->ENG);
+$langue = array("Salut"->FRA, "Hello"->ENG, "Hola"->ESP);
+
