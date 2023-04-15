22c49a979c5b548fa67f97661777f4ce36979bcf
angalland <angalland@laposte.net>
2023-04-15 15:20:27 +0200

    ajout des calcules des divisions euclidienne et modulo

--- exo10.php
+++ exo10.php
@@ -12,12 +12,29 @@ $montpaye = 152;
 $montverse = 200;
 $restepaye = $montverse - $montpaye;
 
-echo "$restepaye";
-$billet = 10;
 
-echo "<br>";
 
-while($restepaye != $billet){
-    echo $billet." <br>";
-    $billet++;
-}
+$bil10 = 10;
+$bil5 = 5;
+$mon2 = 2;
+$mon1 = 1;
+
+$i=$restepaye;
+
+echo "Rendu de monnaie : <br>";
+
+    $i = floor($i/$bil10); //4
+    $m = $restepaye % $bil10; //8
+    echo $i." billet de 10€ ";
+
+    $i1 = floor($m/$bil5); // 1
+    $m2 = $m % $bil5; // 3
+    echo "- $i1 billet de 5€";
+    
+    $i3 = floor($m2/$mon2); //1
+    $m3 = $m2 % $mon2; //1
+    echo " - $i3 piece de 2€";
+ 
+
+    $i4 = floor($m3/$mon1); //1
+    echo " - $i4 piece de 1€";
